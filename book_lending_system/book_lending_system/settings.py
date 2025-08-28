2a8ca961046bf7e887f14ec46d0c95ce4ecab71e
akashuk2003 <akashuk48@gmail.com>
2025-08-28 14:44:16 +0530

    deployment db update

--- book_lending_system/book_lending_system/settings.py
+++ book_lending_system/book_lending_system/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import environ
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,10 +98,7 @@ WSGI_APPLICATION = 'book_lending_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
